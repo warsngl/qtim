7c9e70df72be19fa2d7759633f7a919964486145
warsngl <afshyn@yandex.ru>
2023-09-04 19:43:28 +0300

    init

---
+++ src/components/Home.vue
@@ -0,0 +1,30 @@
+<template lang='pug'>
+h1 Home Page
+.flex.items-center
+  p.mr-4.text-2xl {{ count }}
+  button.rounded-full.px-2(@click='inc') +
+</template>
+
+<script>
+export default {
+  data:()=>({
+
+  }),
+  computed:{
+    count(){
+      return this.$store.state.count
+    }
+  },
+  methods:{
+    inc(){
+      this.$store.commit('increment')
+    }
+  }
+}
+</script>
+
+<style>
+button{
+  background: #7C99AC;
+}
+</style>
